ac1c9ec5ffa84fd428800715777eaf8141482886
Omkar Moghe <theomkarmoghe@gmail.com>
2026-06-21 22:57:51 -0700

    wip

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
-var map = L.map("map");
+var map = L.map("map").setView([34.0522, -118.2437], 10);
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
+  zoomControl: false,
+  maxZoom: 16,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
-map.locate({ setView: true, maxZoom: 16 });
