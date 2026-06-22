73f481b39f4a1fb7f8d0efc3f9fda455fe8dc685
Omkar Moghe <theomkarmoghe@gmail.com>
2026-06-21 21:49:00 -0700

    fix scale, use location

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-var map = L.map("map").setView([51.505, -0.09], 13);
+var map = L.map("map");
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+map.locate({ setView: true, maxZoom: 16 });
