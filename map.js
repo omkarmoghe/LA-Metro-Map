e8624f49e4462d187a70cc209df5b9376da6075f
Omkar Moghe <theomkarmoghe@gmail.com>
2026-06-22 13:55:17 -0700

    better icons

--- map.js
+++ map.js
@@ -1,10 +1,15 @@
-var map = L.map("map").setView([34.0522, -118.2437], 10);
+const map = L.map("map").setView([34.0522, -118.2437], 10);
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   zoomControl: false,
   maxZoom: 16,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+
+const icon = L.divIcon({
+  html: "<span>Ⓜ️</span>",
+  className: "station-icon",
+});
 
 fetch("/geo/lines.geojson")
   .then((response) => response.json())
@@ -15,18 +20,20 @@ fetch("/geo/lines.geojson")
 fetch("/geo/stops.csv")
   .then((response) => response.text())
   .then((csv) => {
-    const lines = csv.split("\n");
-    const headers = lines[0].split(",");
-    const latIdx = headers.indexOf("stop_lat");
-    const lonIdx = headers.indexOf("stop_lon");
-    const nameIdx = headers.indexOf("stop_name");
+    const [headerLine, ...dataLines] = csv.split("\n");
+    const headers = headerLine.split(",");
 
-    for (let i = 1; i < lines.length; i++) {
-      const cols = lines[i].split(",");
-      const lat = parseFloat(cols[latIdx]);
-      const lon = parseFloat(cols[lonIdx]);
-      if (!isNaN(lat) && !isNaN(lon)) {
-        L.marker([lat, lon]).addTo(map).bindPopup(cols[nameIdx]);
-      }
-    }
+    const stops = dataLines
+      .map((line) =>
+        Object.fromEntries(line.split(",").map((v, i) => [headers[i], v])),
+      )
+      .forEach((stop) => {
+        const stopLat = parseFloat(stop.stop_lat);
+        const stopLon = parseFloat(stop.stop_lon);
+        if (isNaN(stopLat) || isNaN(stopLon)) return;
+
+        L.marker([stopLat, stopLon], { icon })
+          .addTo(map)
+          .bindPopup(stop.stop_name);
+      });
   });
