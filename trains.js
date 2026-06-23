27d5227a2957ba77c4b2f49e8d05f0759f068256
Omkar Moghe <theomkarmoghe@gmail.com>
2026-06-22 19:08:11 -0700

    basic shit working babyyy

--- trains.js
+++ trains.js
@@ -2,8 +2,11 @@ const ws = new WebSocket(
   "wss://api.metro.net/ws/LACMTA_Rail/vehicle_positions",
 );
 
+let trainMarkers = {};
+
 ws.addEventListener("message", (event) => {
-  console.log(`Websocket message: ${event.data.length}`);
+  const payload = JSON.parse(event.data);
+  vehicleUpdate(payload);
 });
 
 ws.addEventListener("error", (event) => {
@@ -13,3 +16,20 @@ ws.addEventListener("error", (event) => {
 ws.addEventListener("close", (event) => {
   console.log(`WebSocket closed: ${event.code} ${event.reason}`);
 });
+
+function vehicleUpdate(payload) {
+  if (!Object.hasOwn(trainMarkers, payload.id)) {
+    const trainMarker = document.createElement("span");
+    trainMarker.textContent = "🚈";
+    trainMarkers[payload.id] = new maplibregl.Marker({
+      element: trainMarker,
+    });
+  }
+
+  trainMarkers[payload.id]
+    .setLngLat([
+      payload.vehicle.position.longitude,
+      payload.vehicle.position.latitude,
+    ])
+    .addTo(map);
+}
